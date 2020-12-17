8f2056b241edc4e4c17058ad9280a329e3c598ea
TrailguidePeter <peat_polite@web.de>
2020-12-17 22:36:43 +0100

    Trailgiude logo to ground

--- assets/map/poi/trailguide.js
+++ assets/map/poi/trailguide.js
@@ -1,9 +1,11 @@
 // Add Trailguide logo
 viewer.entities.add({
-  position: Cesium.Cartesian3.fromDegrees(8.845, 60.685, 600),
+  position: Cesium.Cartesian3.fromDegrees(8.845, 60.685, 20),
   billboard: {
     image: "https://res.cloudinary.com/trailguide-as/image/upload/v1607637200/logo/trailguidewhite.png",
     scaleByDistance: new Cesium.NearFarScalar(0, 0.1, 1.5e5, 0.01),
     distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 1.5e5),
+    verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+    heightReference: Cesium.HeightReference.RELATIVE_TO_GROUND,
   },
 });
