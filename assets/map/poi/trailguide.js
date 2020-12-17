40813541070404e2bde6e7c4ca45493d3bb5b3e8
TrailguidePeter <peat_polite@web.de>
2020-12-17 22:41:25 +0100

    tg logo other height

--- assets/map/poi/trailguide.js
+++ assets/map/poi/trailguide.js
@@ -1,6 +1,6 @@
 // Add Trailguide logo
 viewer.entities.add({
-  position: Cesium.Cartesian3.fromDegrees(8.845, 60.685, 20),
+  position: Cesium.Cartesian3.fromDegrees(8.845, 60.685, 150),
   billboard: {
     image: "https://res.cloudinary.com/trailguide-as/image/upload/v1607637200/logo/trailguidewhite.png",
     scaleByDistance: new Cesium.NearFarScalar(0, 0.1, 1.5e5, 0.01),
