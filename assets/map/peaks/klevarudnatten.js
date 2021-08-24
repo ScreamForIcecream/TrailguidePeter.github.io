e90ec7afa037382d88ea0b5bd7fe061c6dae3a28
TrailguidePeter <peat_polite@web.de>
2021-08-24 16:26:25 +0200

    poi

--- assets/map/peaks/klevarudnatten.js
+++ assets/map/peaks/klevarudnatten.js
@@ -27,8 +27,6 @@ Cesium.when(promise, function(updatedPositions) {
             //outlineColor: Cesium.Color.fromBytes(255,255,255,220),
             //outlineWidth: 1,
             //style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-            pixelOffset: new Cesium.Cartesian2(0.0, -facilityHeight),
-            pixelOffsetScaleByDistance: new Cesium.NearFarScalar(0, 0.6, 0.5e5, 0.05),
             scaleByDistance: new Cesium.NearFarScalar(0, 1, 0.5e5, 0.1),
             distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 0.5e5),
             horizontalOrigin: Cesium.HorizontalOrigin.BOTTOM,
