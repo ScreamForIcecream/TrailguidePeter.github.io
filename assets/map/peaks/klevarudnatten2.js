eace4b7dd1e7a6c3d3eb40cc16e6be0d5f5bf462
TrailguidePeter <peat_polite@web.de>
2021-08-24 14:47:42 +0200

    poi

--- assets/map/peaks/klevarudnatten2.js
+++ assets/map/peaks/klevarudnatten2.js
@@ -3,21 +3,27 @@ var terrainProvider = new Cesium.createWorldTerrain();
 var positions = [Cesium.Cartographic.fromDegrees(9.1352287, 60.5873951),];
 var promise = Cesium.sampleTerrainMostDetailed(terrainProvider, positions);
 Cesium.when(promise, function(updatedPositions) {
-  
-viewer.entities.add({
-  position: Cesium.Cartesian3.fromRadians(positions[0].longitude, positions[0].latitude, positions[0].height+40),
-  label: {
-    text: "Kleverudnatten\n808m",
-    font: "24px Arial",
-    fillColor: Cesium.Color.fromBytes(255,255,255),
-    //outlineColor: Cesium.Color.fromBytes(255,255,255,220),
-    //outlineWidth: 1,
-    //style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-    pixelOffset: new Cesium.Cartesian2(0.0, -facilityHeight),
-    pixelOffsetScaleByDistance: new Cesium.NearFarScalar(0, 0.6, 0.5e5, 0.05),
-    scaleByDistance: new Cesium.NearFarScalar(0, 1, 0.5e5, 0.1),
-    distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 0.5e5),
-    horizontalOrigin: Cesium.HorizontalOrigin.BOTTOM,
-    backgroundPadding: new Cesium.Cartesian2(4, 3),
-  },
-});
+
+  Cesium.when.all(
+    function (images) {
+      var facilityHeight = images[0].height;
+
+      viewer.entities.add({
+        position: Cesium.Cartesian3.fromRadians(positions[0].longitude, positions[0].latitude, positions[0].height+40),
+        label: {
+          text: "Kleverudnatten\n808m",
+          font: "24px Arial",
+          fillColor: Cesium.Color.fromBytes(255,255,255),
+          //outlineColor: Cesium.Color.fromBytes(255,255,255,220),
+          //outlineWidth: 1,
+          //style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+          pixelOffset: new Cesium.Cartesian2(0.0, -facilityHeight),
+          pixelOffsetScaleByDistance: new Cesium.NearFarScalar(0, 0.6, 0.5e5, 0.05),
+          scaleByDistance: new Cesium.NearFarScalar(0, 1, 0.5e5, 0.1),
+          distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 0.5e5),
+          horizontalOrigin: Cesium.HorizontalOrigin.BOTTOM,
+          backgroundPadding: new Cesium.Cartesian2(4, 3),
+        },
+      });
+      });
+    });
